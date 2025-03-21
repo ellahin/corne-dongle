4c18e3c08599dfdd213eb4b6b7b6fef9c4058e03
Ella Hinschen <102624759+ellahin@users.noreply.github.com>
2025-03-21 16:28:40 +1100

    fixed image

--- config/boards/shields/nice_view_custom/widgets/art.c
+++ config/boards/shields/nice_view_custom/widgets/art.c
@@ -371,7 +371,7 @@ const LV_ATTRIBUTE_MEM_ALIGN LV_ATTRIBUTE_LARGE_CONST LV_ATTRIBUTE_IMG_CREATURE 
 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x20, 0x00, 0x00, 0x00, 
 };
 
-const lv_img_dsc_t Creature = {
+const lv_img_dsc_t creature = {
         .header.cf = LV_IMG_CF_INDEXED_1BIT,
         .header.always_zero = 0,
         .header.reserved = 0,
